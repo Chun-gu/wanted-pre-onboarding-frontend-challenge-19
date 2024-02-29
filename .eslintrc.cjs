ffc426776cb68741cf887d36617b8346c0e0881f
Chun-gu <chungu.yi92@gmail.com>
2024-03-01 05:16:38 +0900

    style: import 관련 규칙 설정 및 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
 		react: {
 			version: "detect",
 		},
+		"import/resolver": { typescript: true, node: true },
 	},
 	extends: [
 		"eslint:recommended",
@@ -13,6 +14,8 @@ module.exports = {
 		"plugin:react/recommended",
 		"plugin:react/jsx-runtime",
 		"plugin:react-hooks/recommended",
+		"plugin:import/recommended",
+		"plugin:import/typescript",
 		"prettier",
 	],
 	ignorePatterns: ["dist", ".eslintrc.cjs"],
@@ -28,6 +31,36 @@ module.exports = {
 		"react-refresh/only-export-components": [
 			"warn",
 			{ allowConstantExport: true },
+		],
+		"import/order": [
+			"error",
+			{
+				groups: [
+					"builtin",
+					"external",
+					"internal",
+					["parent", "sibling", "index"],
+					"type",
+				],
+				pathGroups: [
+					{
+						pattern: "react{*/**}",
+						group: "builtin",
+						position: "after",
+					},
+					{
+						pattern: "@/**",
+						position: "after",
+						group: "external",
+					},
+				],
+				"newlines-between": "always",
+				pathGroupsExcludedImportTypes: [],
+				alphabetize: {
+					order: "asc",
+					caseInsensitive: true,
+				},
+			},
 		],
 	},
 };
